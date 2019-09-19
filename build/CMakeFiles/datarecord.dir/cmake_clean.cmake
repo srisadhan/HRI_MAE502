7a98990b66340c2e557d2b2fc6f33ab64391c0fc
srisadhan <srisadha@buffalo.edu>
2019-09-19 11:59:23 -0400

    handover files

---
+++ build/CMakeFiles/datarecord.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/datarecord.dir/src/datarecord.cpp.o"
+  "../bin/datarecord.pdb"
+  "../bin/datarecord"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/datarecord.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/gripper_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gripper_demo.dir/src/gripper_demo.cpp.o"
+  "../bin/gripper_demo.pdb"
+  "../bin/gripper_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gripper_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
